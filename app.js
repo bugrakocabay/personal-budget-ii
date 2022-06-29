f3298d190cd01363a4710aa1ef5ff252f6e45761
peonola <kocabaybugra@gmail.com>
2022-06-29 14:18:36 +0300

    add documentation

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const envRouter = require("./routes/envelopeRouter");
 const transactionRouter = require("./routes/transactionRouter");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./openapi.json");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/v1/envelopes", envRouter);
 app.use("/api/v1/transactions", transactionRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(4000, () => {
   console.log("App running on port 4000!");
